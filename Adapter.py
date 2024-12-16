815b2d4331d8a57bed3d41d8a73303f5b8eef342
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2024-12-16 12:44:39 +0300

    Lab2 Task6 Паттерн Адаптер

--- Adapter.py
+++ Adapter.py
@@ -1,17 +1,17 @@
-from DriverRepBase import  DriverRepBase
+from DriverRepository import  DriverRepository
 class Adapter:
-    def __init__(self, driver_rep_base: DriverRepBase):
-        self._driver_rep_base = driver_rep_base
+    def __init__(self, driver_rep: DriverRepository):
+        self._driver_rep = driver_rep
     def get_k_n_short_list(self, k, n):
-        return self._driver_rep_base.get_k_n_short_list(k, n)
+        return self._driver_rep.get_k_n_short_list(k, n)
     def get_by_id(self, driver_id):
-        return self._driver_rep_base.get_by_id(driver_id)
+        return self._driver_rep.get_by_id(driver_id)
     def delete_by_id(self, driver_id):
-        self._driver_rep_base.delete_by_id(driver_id)
+        self._driver_rep.delete_by_id(driver_id)
         
     def update_by_id(self, driver_id, updates: dict):
-        self._driver_rep_base.replace_by_id(driver_id, updates)
+        self._driver_rep.replace_by_id(driver_id, updates)
     def add(self, driver: dict):
-        self._driver_rep_base.add_student(driver)
+        self._driver_rep.add_student(driver)
     def get_count(self):
-        return self._driver_rep_base.get_count()
+        return self._driver_rep.get_count()
