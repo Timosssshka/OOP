36dfc08c93adb7b411243031db79d68e1a14dc11
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2024-12-18 02:43:15 +0300

    Lab2 Task3 Создание стратегии для работы с json и yaml

--- DriverStrategy.py
+++ DriverStrategy.py
@@ -4,15 +4,12 @@ class DriverStrategy(ABC):
 
     @abstractmethod
     def read(self):
-      """Загрузка данных из файла."""
         pass
       
     @abstractmethod
     def write(self, data):
-        """Сохранение данных в файл."""
         pass
 
     @abstractmethod
     def display(self):
-        """Отображает содержимое файла."""
         pass
