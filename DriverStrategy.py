278998752a17882438f9acac9c006d789172dc5b
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2024-12-21 01:10:51 +0300

    Update DriverStrategy.py

--- DriverStrategy.py
+++ DriverStrategy.py
@@ -1,15 +1,1 @@
-from abc import ABC, abstractmethod
 
-class DriverStrategy(ABC):
-
-    @abstractmethod
-    def read(self):
-        pass
-      
-    @abstractmethod
-    def write(self, data):
-        pass
-
-    @abstractmethod
-    def display(self):
-        pass
