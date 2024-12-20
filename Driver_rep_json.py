5805e16e10bbad7b8c046b7c3830f6a484362da6
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2024-12-21 01:52:01 +0300

    Создание Driver_rep_json.py

--- Driver_rep_json.py
+++ Driver_rep_json.py
@@ -1,1 +1,21 @@
+import json
+import os
+
+class DriverRepJson:
+     def __init__(self,json_file):
+        self.json_file = json_file
+
+    def read(self):
+        if not os.path.exists(self.json_file):
+            return []
+        with open(self.json_file, 'r') as file:
+           data = json.load(f
+            return data
+
+    def write(self, data):
+        with open(self.json_file, 'w') as file:
+            json.dump(data, file)
+
+strategy = DriverRepJson('driver.json')
+json_repository = DriverRepository(strategy)
 
