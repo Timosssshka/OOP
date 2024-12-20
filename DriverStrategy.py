4d4934e5c019a0114c47a3c4acfa431792d018aa
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2024-12-21 01:53:35 +0300

    Создал DriverStrategy.py

--- DriverStrategy.py
+++ DriverStrategy.py
@@ -1,1 +1,12 @@
 
+from abc import ABC, abstractmethod
+
+class DriverStrategy(ABC):
+
+    @abstractmethod
+    def read(self):
+        pass
+      
+    @abstractmethod
+    def write(self, data):
+        pass
